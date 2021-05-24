cc4c0260fe9c53349a17bc84ed8497805b0ca866
李藜 <lili2018@xinhua.org>
2021-05-24 17:34:33 +0800

    vite搭建

---
+++ cases/ts/viteTest/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "plugin:@typescript-eslint/recommended",
+        'plugin:prettier/recommended' // 添加 prettier 插件
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
